c9d4f29e97c88414770c6c174a496775d79c6336
Juhplux <68930642+Juhplux@users.noreply.github.com>
2025-05-22 02:12:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,166 @@
+const line1 = document.getElementById("L1");
+const line2 = document.getElementById("L2");
+const line3 = document.getElementById("L3");
+const line4 = document.getElementById("L4");
+const line5 = document.getElementById("L5");
+const line6 = document.getElementById("L6");
+const line7 = document.getElementById("L7");
+const line8 = document.getElementById("L8");
+const line9 = document.getElementById("L9");
+const line10 = document.getElementById("L10");
+const line11 = document.getElementById("L11");
+const line12 = document.getElementById("L12");
+const line13 = document.getElementById("L13");
+const line14 = document.getElementById("L14");
+const line15 = document.getElementById("L15");
+const line16 = document.getElementById("L16");
+const line17 = document.getElementById("L17");
+const line18 = document.getElementById("L18");
+const line19 = document.getElementById("L19");
+const line20 = document.getElementById("L20");
+const line21 = document.getElementById("L21");
+const line22 = document.getElementById("L22");
+const line23 = document.getElementById("L23");
+
+
+setTimeout(function() {
+  
+  line1.style.visibility='visible';
+ 
+}, 2000);
+
+setTimeout(function() {
+  
+  line2.style.visibility='visible';
+ 
+}, 5700);
+
+setTimeout(function() {
+  
+  line3.style.visibility='visible';
+ 
+}, 8500);
+
+setTimeout(function() {
+  
+  line4.style.visibility='visible';
+ 
+}, 12500);
+
+
+setTimeout(function() {
+  
+  line5.style.visibility='visible';
+ 
+}, 16500);
+
+setTimeout(function() {
+  
+  line6.style.visibility='visible';
+ 
+}, 19500);
+
+setTimeout(function() {
+  
+  line7.style.visibility='visible';
+ 
+}, 23000);
+
+setTimeout(function() {
+  
+  line8.style.visibility='visible';
+ 
+}, 26500);
+
+
+setTimeout(function() {
+  
+  line9.style.visibility='visible';
+ 
+}, 28500);
+
+
+setTimeout(function() {
+  
+  line10.style.visibility='visible';
+ 
+}, 32500);
+
+setTimeout(function() {
+  
+  line11.style.visibility='visible';
+ 
+}, 36500);
+
+
+setTimeout(function() {
+  
+  line12.style.visibility='visible';
+ 
+}, 41000);
+
+setTimeout(function() {
+  
+  line13.style.visibility='visible';
+ 
+}, 42500);
+
+setTimeout(function() {
+  
+  line14.style.visibility='visible';
+ 
+}, 46500);
+
+setTimeout(function() {
+  
+  line15.style.visibility='visible';
+ 
+}, 51500);
+
+setTimeout(function() {
+  
+  line16.style.visibility='visible';
+ 
+}, 55500);
+
+setTimeout(function() {
+  
+  line17.style.visibility='visible';
+ 
+}, 60000);
+
+setTimeout(function() {
+  
+  line18.style.visibility='visible';
+ 
+}, 64000);
+
+setTimeout(function() {
+  
+  line19.style.visibility='visible';
+ 
+}, 67500);
+
+setTimeout(function() {
+  
+  line20.style.visibility='visible';
+ 
+}, 76000);
+
+setTimeout(function() {
+  
+  line21.style.visibility='visible';
+ 
+}, 77000);
+
+setTimeout(function() {
+  
+  line22.style.visibility='visible';
+ 
+}, 80000);
+
+setTimeout(function() {
+  
+  line23.style.visibility='visible';
+ 
+}, 83000);
